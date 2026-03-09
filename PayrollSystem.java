7c7f817d1f587d9d4c79ff36c828f0be3d15f594
Chris Brilianto Chandra <rejuvenation@Chriss-MacBook-Pro.local>
2026-03-09 15:59:43 +0800

    Fixed a typo resulting in other class being nested in Manager()

--- PayrollSystem.java
+++ PayrollSystem.java
@@ -1,11 +1,18 @@
 public class PayrollSystem {
 
         public static void main(String[] args) {   
+
+        Employee[] employees = new Employee[4];
+        employees[0] = new Manager("Alya", 5000000);
+        employees[1] = new Programmer("Budi", 4000000);
+        employees[2] = new Programmer("Citra", 4500000);
+        employees[3] = new Intern("Dina", 2000000);
+
+        for (Employee employee : employees) {
+            employee.printInfo();
         }
 
-
 }
-
 
 class Employee {
     public String name;
@@ -40,7 +47,7 @@ class Manager extends Employee {
     public String getRole() {
         return "Manager";
 }
-
+}
 class Programmer extends Employee {
     public Programmer(String name, double baseSalary) {
         super(name, baseSalary);
