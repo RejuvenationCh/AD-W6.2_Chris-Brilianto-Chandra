dcefd1f0638a78a7480a18fc3af2e6d341863d6f
Chris Brilianto Chandra <rejuvenation@Chriss-MacBook-Pro.local>
2026-03-09 15:54:55 +0800

    Added Employee and their classes

---
+++ PayrollSystem.java
@@ -0,0 +1,71 @@
+public class PayrollSystem {
+
+        public static void main(String[] args) {   
+        }
+
+
+}
+
+
+class Employee {
+    public String name;
+    public double baseSalary;
+    public Employee(String name, double baseSalary) {
+        this.name = name;
+        this.baseSalary = baseSalary;
+    }
+        public void printInfo() {
+            System.out.println(name);
+         }
+
+         public String getRole() {
+            return "Employee";
+         }
+        public double calculateSalary() {
+            return baseSalary;
+        }
+}
+
+class Manager extends Employee {
+    public Manager(String name, double baseSalary) {
+        super(name, baseSalary);
+    }
+    @Override
+    public double calculateSalary() {
+        return baseSalary + 2000000;
+    }
+    @Override
+    public String getRole() {
+        return "Manager";
+}
+
+class Programmer extends Employee {
+    public Programmer(String name, double baseSalary) {
+        super(name, baseSalary);
+    }
+    @Override
+    public double calculateSalary() {
+        return baseSalary + 1000000;
+    }
+    @Override
+    public String getRole() {
+        return "Programmer";
+    }
+
+}
+
+class Intern extends Employee {
+    public Intern(String name, double baseSalary) {
+        super(name, baseSalary);
+    }
+    @Override
+    public double calculateSalary() {
+        return baseSalary + 300000;
+    }
+    @Override
+    public String getRole() {
+        return "Intern";
+    }
+
+}
+}
