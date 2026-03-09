aa117d6079df29ba9441bf63f1d3ac4b22ad70be
Chris Brilianto Chandra <rejuvenation@Chriss-MacBook-Pro.local>
2026-03-09 16:05:14 +0800

    Added the employees & printInfo function

--- PayrollSystem.java
+++ PayrollSystem.java
@@ -3,15 +3,15 @@ public class PayrollSystem {
         public static void main(String[] args) {   
 
         Employee[] employees = new Employee[4];
-        employees[0] = new Manager("Alya", 5000000);
-        employees[1] = new Programmer("Budi", 4000000);
-        employees[2] = new Programmer("Citra", 4500000);
-        employees[3] = new Intern("Dina", 2000000);
+            employees[0] = new Manager("Alya", 5000000);
+            employees[1] = new Programmer("Budi", 4000000);
+            employees[2] = new Programmer("Citra", 4500000);
+            employees[3] = new Intern("Dina", 2000000);
 
         for (Employee employee : employees) {
             employee.printInfo();
         }
-
+}
 }
 
 class Employee {
@@ -26,7 +26,7 @@ class Employee {
             System.out.println(getRole());
             System.out.println(calculateSalary());
          }
-
+         
          public String getRole() {
             return "Employee";
          }
@@ -74,6 +74,5 @@ class Intern extends Employee {
     public String getRole() {
         return "Intern";
     }
+}
 
-}
-}
